c8d7aedff0a4e1cfd4a1fa67340bc41bc85aafcf
tropireep <redvesa8@gmail.com>
2026-04-15 13:18:50 +0300

    upload my project

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Волейбольний менеджер. Офіційний сайт.</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <header class="header">
+            <div class="logo">
+                <div class="logo-text">
+                    <p>Волейбольний</p>
+                    <b>Менеджер</b>
+                    <i>Офіційний сайт</i>
+                </div>
+                <picture>
+                    <img src="https://i.pinimg.com/736x/1a/4f/2d/1a4f2dc37d2b9f459af6f3807eecdfb6.jpg" alt="логотип волейбольного менеджера" width="100">
+                </picture>
+            </div>
+            
+            <div class="contacts">
+                <a href="tel:+380975555501">+38 (097) 555-55-01</a>
+                <i>Працюємо 24/7</i>
+            </div>
+        </header> <main>
+            <picture class="main-picture">
+                <img src="https://i.pinimg.com/1200x/08/6f/65/086f653bed3bfed35e7dcf32dfa5f5e5.jpg" alt="Про волейбол сітка">
+            </picture>
+            
+            <div class="main">
+                <section class="main-text">
+                    <h1>Про волейбол</h1>
+                    <p>Командний вид спорту, який грається між двома командами по шість гравців зі сферичним м'ячем. Понад 800 мільйонів людей у всьому світі грають у волейбол принаймні раз на тиждень, що робить його одним із найпопулярніших видів спорту.</p>
+                    <p>У волейбол грають на прямокутному майданчику, розділеному сіткою. Мета гри полягає в тому, щоб спрямувати м'яч через сітку таким чином, щоб він торкнувся підлоги на боці суперника, або змусити суперника припуститися помилки.</p>
+                    <p>Гравцям дозволяється торкатися м'яча будь-якою частиною тіла, але не можна його затримувати або кидати. Кожна команда має право торкнутися м'яча не більше трьох разів, перш ніж перекинути його на сторону супротивника.</p>
+                </section>
+                
+                <aside class="menu">
+                    <p>Волейбольні партнери</p>
+                    <nav>
+                        <ul>
+                            <li><a href="#">ОСТАННІ НОВИНИ</a></li>
+                            <li><a href="#">ПАРІ МАТЧ</a></li>
+                            <li><a href="#">ПРИВАТНИЙ МЕНЕДЖЕР ВАСЯ</a></li>
+                            <li><a href="#">ФАНАТИ СЕКТОРУ №12</a></li>
+                            <li><a href="#">СПОРТИВНЕ ХАРЧУВАННЯ</a></li>
+                        </ul>
+                    </nav>
+                </aside>
+            </div>
+        </main>
+        
+        <footer> 
+            <p class="footer">© Волейбольний менеджер 2026.</p>
+        </footer>
+    </div>
+</body>
+</html>
